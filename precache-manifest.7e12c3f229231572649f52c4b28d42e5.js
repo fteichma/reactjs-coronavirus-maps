cf49fd72de54dd7a2cc3ffbb656afeaf04dc58d2
Fabian Teichmann <fteichma@MacBook-Pro-de-Fabian.local>
2020-03-25 13:21:49 +0100

    Updates

---
+++ precache-manifest.7e12c3f229231572649f52c4b28d42e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b2b4c439591fa9e9e90bf7391f945db",
+    "url": "/reactjs-coronavirus-maps/index.html"
+  },
+  {
+    "revision": "a812a2138677e4c87722",
+    "url": "/reactjs-coronavirus-maps/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "76c4a0392382b5a54dd9",
+    "url": "/reactjs-coronavirus-maps/static/css/main.929ecc97.chunk.css"
+  },
+  {
+    "revision": "a812a2138677e4c87722",
+    "url": "/reactjs-coronavirus-maps/static/js/2.9b42aec9.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "/reactjs-coronavirus-maps/static/js/2.9b42aec9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76c4a0392382b5a54dd9",
+    "url": "/reactjs-coronavirus-maps/static/js/main.62588408.chunk.js"
+  },
+  {
+    "revision": "31cf46d15cf8356c4f47",
+    "url": "/reactjs-coronavirus-maps/static/js/runtime-main.a6077f99.js"
+  }
+]);
