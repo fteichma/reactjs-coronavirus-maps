bcf396370c3c99be249b9caea918f20ba9eca0c2
Fabian Teichmann <fteichma@MacBook-Pro-de-Fabian.local>
2020-03-27 02:43:02 +0100

    Cases_ratio => Selected (cases/active/recovered/.)

--- src/App.js
+++ src/App.js
@@ -4,16 +4,16 @@ import worldGeoJSON from 'geojson-world-map';
 import './styles.css';
 import countryCode from './countrycode-latlong';
 import "leaflet/dist/leaflet.css";
+import { ChevronDown } from 'react-feather';
 
 import L from "leaflet";
 
 // SVG to URL
-const url = "data:image/svg+xml,%3Csvg width='50' height='50' viewBox='0 0 50 50' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Ccircle cx='25' cy='25' r='24' fill='%23a9002a' fill-opacity='0.6' stroke='%23a9002a' stroke-width='2'/%3E%3C/svg%3E%0A";
 
-const customMarker = (ratio) => new L.icon({
-  iconUrl: url,
-  iconSize: 2 * ratio,
-  iconAnchor: [1 * ratio, 1 * ratio],
+const customMarker = (ratio, rgb) => new L.icon({
+  iconUrl: "data:image/svg+xml,%3Csvg width='50' height='50' viewBox='0 0 50 50' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Ccircle cx='25' cy='25' r='24' fill='%23" + rgb + "' fill-opacity='0.6' stroke='%23" + rgb + "' stroke-width='2'/%3E%3C/svg%3E%0A",
+  iconSize: 5 * ratio,
+  iconAnchor: [2.5 * ratio, 2.5 * ratio],
 });
 
 const axios = require('axios');
@@ -21,8 +21,9 @@ const axios = require('axios');
 class App extends Component {
   constructor() {
     super();
+    this.handleChange = this.handleChange.bind(this);
     this.state = {
-      selected: null,
+      selected: "cases",
       data: [],
       dataTotal: [],
       center: [0, 0],
@@ -30,8 +31,11 @@ class App extends Component {
     };
   }
 
-  getData() {
+  handleChange(event) {
+    this.setState({ selected: event.target.value });
+  }
 
+  getData() {
     // Cases by country
     axios.get("https://coronavirus-monitor.p.rapidapi.com/coronavirus/cases_by_country.php", {
       "headers": {
@@ -99,21 +103,41 @@ class App extends Component {
           />
           {this.state.data.map((value, index) => {
             let cases = value.cases.replace(/,/g, "");
-            let cases_ratio = Math.pow(cases, 0.28);
+            let recovered = value.total_recovered.replace(/,/g, "");
+            let active = value.active_cases.replace(/,/g, "");
+            let deaths = value.deaths.replace(/,/g, "");
+            var cases_ratio = 0;
+            var rgb = "";
+            if (this.state.selected === "cases") {
+              cases_ratio = Math.pow(cases, 0.2);
+              rgb = "a9002a";
+            }
+            else if (this.state.selected === "active") {
+              cases_ratio = Math.pow(active, 0.2);
+              rgb = "ffc107";
+            }
+            else if (this.state.selected === "recovered") {
+              cases_ratio = Math.pow(recovered, 0.2);
+              rgb = "28a745";
+            }
+            else {
+              cases_ratio = Math.pow(deaths, 0.2);
+              rgb = "9c9c9c";
+            }
             return (
               (countryCode.find((el) => el.name === value.country_name)) ?
                 (<Marker key={index} position={
                   countryCode.find((el) => el.name === value.country_name) &&
                   countryCode.find((el) => el.name === value.country_name).latlng
-                } icon={customMarker(cases_ratio)}>
+                } icon={customMarker(cases_ratio, rgb)}>
                   <Popup className={'popup'} style={{ backgroundColor: "black" }}>
                     <h1>{value.country_name}</h1>
                     <p><b>Cases: </b> <span style={{ color: '#A90000' }}>{value.cases}</span></p>
-                    <p><b>Today cases: </b> <span style={{ color: '#A90000' }}>{value.new_cases}</span></p>
+                    <p><b>New cases: </b> <span style={{ color: '#A90000' }}>{value.new_cases}</span></p>
                     <p><b>Recovered: </b> <span style={{ color: '#28a745' }}>{value.total_recovered}</span></p>
                     <p><b>Active: </b> <span style={{ color: '#ffc107' }}>{value.active_cases}</span></p>
                     <p><b>Deaths: </b> {value.deaths}</p>
-                    <p><b>Today deaths: </b> {value.new_deaths}</p>
+                    <p><b>New deaths: </b> {value.new_deaths}</p>
                     <p><b>Critical: </b> {value.serious_critical}</p>
                     <p><b>Per 1m: </b>{value.total_cases_per_1m_population}</p>
                   </Popup>
@@ -123,14 +147,46 @@ class App extends Component {
           })
           }
         </LeafletMap>
+        <div id="attributions">
+          &copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors |
+          Data from <a href="https://coronavirus-monitor.p.rapidapi.com/">rapidapi</a> |
+          I am not responsible for the veracity of the information provided |
+          Developed by: <a href="https://github.com/fteichma/">Fabian Teichmann</a>
+        </div>
+        <div className="info-box" id="choices_desktop">
+          <ul>
+            <li className={this.state.selected === "cases" ? "active" : undefined} onClick={() => { this.setState({ selected: "cases" }) }}>
+              Cases
+            </li>
+            <li className={this.state.selected === "active" ? "active" : undefined} onClick={() => { this.setState({ selected: "active" }) }}>
+              Active
+            </li>
+            <li className={this.state.selected === "recovered" ? "active" : undefined} onClick={() => { this.setState({ selected: "recovered" }) }}>
+              Recovered
+            </li>
+            <li className={this.state.selected === "deaths" ? "active" : undefined} onClick={() => { this.setState({ selected: "deaths" }) }}>
+              Deaths
+            </li>
+          </ul>
+        </div>
         <div className="info-box" id="total">
           <p><b>Total cases: </b><span style={{ color: '#A90000' }}>{this.state.dataTotal.total_cases}</span></p>
           <p><b>New cases: </b><span style={{ color: '#A90000' }}>{this.state.dataTotal.new_cases}</span></p>
           <p><b>Total recovered: </b><span style={{ color: '#28a745' }}>{this.state.dataTotal.total_recovered}</span></p>
           <p><b>Total deaths: </b><span>{this.state.dataTotal.total_deaths}</span></p>
           <p><b>New deaths: </b><span>{this.state.dataTotal.new_deaths}</span></p>
-          <p><small>Developed by : <a href="https://github.com/fteichma/">Fabian Teichmann</a></small></p>
-          <p><small>Data from <a href="https://coronavirus-monitor.p.rapidapi.com/">rapidapi</a> - I am not responsible for the veracity of the information provided</small></p>
+          <div className="select">
+            <label htmlFor="select"><b>Choose a view:</b></label>
+            <div id="select">
+              <select value={this.state.selected} name="select" onChange={this.handleChange}>
+                <option value="cases">Cases</option>
+                <option value="active">Active</option>
+                <option value="recovered">Recovered</option>
+                <option value="deaths">Deaths</option>
+              </select>
+              <ChevronDown size={"18px"} />
+            </div>
+          </div>
         </div>
         <div id="gh">
           <p>
@@ -139,7 +195,8 @@ class App extends Component {
             </a>
           </p>
         </div>
-      </div>
+
+      </div >
     );
   }
 }
